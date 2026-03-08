a3e09c48a456d77796986ae02986d921d07a6140
Ashera Trembath <trembathrk@gmail.com>
2026-03-07 20:59:59 -0700

    RIGHT NOW - Cartman

--- services/urls.py
+++ services/urls.py
@@ -6,7 +6,6 @@ from . import views
 app_name = "services"
 
 urlpatterns = [
-	path("", views.index, name="index"),
     path("all/", views.allServices, name="all_services"),
     path('<uuid:company_id>/', views.displayServices, name='company_services'),
 ]
--- services/views.py
+++ services/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import render, get_object_or_404
 from .models import Service, ServiceRequest
 from accounts.models import CompanyAccount
 
-# Create your views here.
-def index(request):
-	return render(request, "services/index.html")
-
 def displayServices(request, company_id):
     # Fetch the company, 404 if it doesn’t exist
     company = get_object_or_404(CompanyAccount, company_id=company_id)
