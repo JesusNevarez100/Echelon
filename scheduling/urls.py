f945414677d267cb0ff7e9a9e2488ed1b6a7073e
Rory Tiernan <102919746+64Dreams@users.noreply.github.com>
2026-03-11 16:58:56 -0600

    crashing out

--- scheduling/urls.py
+++ scheduling/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "scheduling"
 
 urlpatterns = [
-	path("", views.index, name="index")
+	path("", views.index, name="index"),
+    path("api/meetings/", views.meetings_json, name="meetings_json"),
+    path("api/meetings/create/", views.create_meeting, name="create_meeting")
 ]
-
