e328bb13314018f25bbbd384c0adb73d1061570a
Rory Tiernan <102919746+64Dreams@users.noreply.github.com>
2026-03-11 16:59:26 -0600

    crashing out

--- scheduling/views.py
+++ scheduling/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Meeting
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.dateparse import parse_datetime
+import json
 
 # Create your views here.
 def index(request):
 	return render(request, "scheduling/index.html")
+
+
+# Meeting request format
+def meetings_json(request):
+    meetings = Meeting.objects.all()
+
+    events = []
+    for m in meetings:
+        events.append({
+            "id": m.id,
+            "title": m.title,
+            "start": m.start_at.isoformat(),
+            "end": m.end_at.isoformat(),
+        })
+
+    return JsonResponse(events, safe=False)
+
+#Meeting creator
+@csrf_exempt
+def create_meeting(request):
+    attendees = data.get("attendees", "")
+    if request.method == "POST":
+        data = json.loads(request.body)
+
+        meeting = Meeting.objects.create(
+            title=data["title"],
+            start_at=parse_datetime(data["start"]),
+            end_at=parse_datetime(data["end"]),
+            organizer=request.user if request.user.is_authenticated else None,
+            company=request.user.companyaccount if request.user.is_authenticated else None,
+        )
+
+        return JsonResponse({"status": "ok", "id": meeting.id})
